026e6e744766c117dfb2bf418c2d6c3864c4c90f
Guilherme Costantino Maciel <165477002+MacielG@users.noreply.github.com>
2025-11-06 12:53:16 -0300

    amplify debug

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,8 +3,10 @@
 // Carrega as variáveis de ambiente do .env.local para o processo do Next.js
 require('dotenv').config({ path: './.env.local' });
 
+const { withAmplifyAdapter } = require('@aws-amplify/adapter-nextjs');
+
 const nextConfig = {
-  output: 'standalone',
+  // Configurações do Next.js
 };
 
-module.exports = nextConfig;
+module.exports = withAmplifyAdapter(nextConfig);
